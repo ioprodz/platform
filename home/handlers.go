a49ab48caf21e3b80b86b1d3dcfdcd0722f0498e
Osman Kalache <osminosm@gmail.com>
2024-03-04 16:42:59 +0100

    home hide login form when authed

--- home/handlers.go
+++ home/handlers.go
@@ -1,12 +1,15 @@
 package home
 
 import (
+	"ioprodz/common/policies"
 	"ioprodz/common/ui"
 	"net/http"
 )
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 
-	ui.RenderPage(w, r, "home/template", nil)
+	user := r.Context().Value(policies.CurrentUserCtxKey).(policies.CurrentUser)
+
+	ui.RenderPage(w, r, "home/template", map[string]interface{}{"IsAuthenticated": user.IsAuthenticated()})
 
 }
